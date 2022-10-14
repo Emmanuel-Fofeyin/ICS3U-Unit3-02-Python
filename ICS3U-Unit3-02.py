19872b286267e903a29cb3d0d14ca8899ac0f250
Emmanuel Fofeyin Ngwamunwi <emmanuel.fofeyin-ngwamunwi@mths.ca>
2022-10-14 15:04:50 +0000

    made changes

---
+++ ICS3U-Unit3-02.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+# Created by: Emmanuel
+# Created on: Sep 2022
+# This program is a number guessing game
+
+
+import constants
+
+
+def main():
+
+    # input
+    guess_number = int(input("Guess a number from 0 - 9: "))
+
+    # process and output
+    if guess_number == constants.CONSTANT_NUMBER:
+        print("\nYou guessed right.")
+    if guess_number != constants.CONSTANT_NUMBER:
+        print("\nYou guessed wrong, try again.")
+
+    print("\n\nDone.")
+
+
+if __name__ == "__main__":
+    main()
